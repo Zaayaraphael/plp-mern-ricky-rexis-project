651f0a53bd2ac68ceffaca285e6b00b930b5479e
Zaayaraphael <raphaelzaaya1@gmail.com>
2025-11-27 20:05:58 +0100

    resolve usecontext import from Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext';
+import { useAuth } from '../contexts/authContext';
 
 const Header = () => {
   const { user, isAuthenticated, logout } = useAuth();
